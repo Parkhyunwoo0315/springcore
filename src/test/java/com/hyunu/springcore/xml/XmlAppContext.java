f7797d13131c88a608c8dc9b17f78d0be45321fd
Park HyunWoo <roy040315@gmail.com>
2026-07-24 02:01:53 +0900

    feat: xml 기반 설정 #10

---
+++ src/test/java/com/hyunu/springcore/xml/XmlAppContext.java
@@ -0,0 +1,24 @@
+package com.hyunu.springcore.xml;
+
+import com.hyunu.springcore.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+
+    void xmlAppContext() {
+
+        ApplicationContext ac =
+                new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService =
+                ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
