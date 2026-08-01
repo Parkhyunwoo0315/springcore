f853f5227503953dfdbba2ce564a7048fc8ea1fc
Park HyunWoo <roy040315@gmail.com>
2026-08-01 17:03:10 +0900

    test: 조회한 빈이 모두 필요한 경우 List, Map 테스트 close #18

--- src/main/java/com/hyunu/springcore/discount/FixDiscountPolicy.java
+++ src/main/java/com/hyunu/springcore/discount/FixDiscountPolicy.java
@@ -4,6 +4,7 @@ import com.hyunu.springcore.member.Grade;
 import com.hyunu.springcore.member.Member;
 import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;   //1000원 할인
